2fa0b6b00d6918a7abdac6f6fb1f9451d2dbf5dc
wangpeng <wp20150703@163.com>
2016-10-09 19:14:00 +0800

    update

--- Assets/Shader/Animation/AnimationTextureManager.cs
+++ Assets/Shader/Animation/AnimationTextureManager.cs
@@ -4,5 +4,21 @@ using UnityEngine.UI;
 
 public class AnimationTextureManager : MonoBehaviour {
 
-	public Slider slider
+	public Slider sliderSpeed;
+
+	public Material materialTexture;
+
+	public void SpeedChanged(){
+		if(null != sliderSpeed){
+			materialTexture.SetFloat("_Speed", sliderSpeed.value);
+		}
+	}
+
+	public void JumpGit(){
+		Application.OpenURL("");
+	}
+
+	public void GoToMainMenu(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene("ChoiceFunctionMenu");
+	}
 }
