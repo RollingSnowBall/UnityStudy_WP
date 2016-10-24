57a30425b52b5fe76a044fb3c60b0cdd0d2ba357
wangpeng <wp20150703@163.com>
2016-10-24 12:18:49 +0800

    布料系统展示

---
+++ Assets/FunctionTest/Cloth/ClothManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using I2.Loc;
+using UnityEngine.SceneManagement;
+
+public class ClothManager : Singleton<ClothManager> {
+
+	public Cloth cloth;
+	public Text textStart;
+
+	public Slider sliderDamping;
+
+	private bool bIsStart;//开始或者重新开始
+
+	public void start(){
+		if(!bIsStart){
+			if(null != cloth){
+				cloth.useGravity = true;
+			}
+			if(null != textStart){
+				textStart.text = ScriptLocalization.Get("startRestart");
+			}
+		}else{
+			SceneManager.LoadScene("FunctionTest_Cloth_Scene");
+		}
+		bIsStart = !bIsStart;
+	}
+
+	public void changeDamping(){
+		if(null != sliderDamping && null != cloth){
+			cloth.damping = sliderDamping.value;
+		}
+	}
+}
