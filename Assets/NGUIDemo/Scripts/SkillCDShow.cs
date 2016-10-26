a0354d1dbecfc10e3c842d63bc0c88b6ee8dca83
wangpeng <wp20150703@163.com>
2016-10-26 16:25:32 +0800

    删除无用文件

---
+++ Assets/NGUIDemo/Scripts/SkillCDShow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillCDShow : MonoBehaviour {
+
+	public UISprite spriteSkillWhile;
+	public UISprite spriteSkillUpdate;
+
+	public void ShowSkillCD(){
+		
+	}
+}
--- ProjectSettings/Assets/Scripts/Config.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Config {
-
-	public static string szSaderScrollingBackgroundCodeURL = "https://github.com/RollingSnowBall/UnityStudy_WP/tree/Shader/develop/Assets/Shader/ScrollingBackground";
-}
